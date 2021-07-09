2f4f916636c74be6fa6c76d5d323aa7456ef41fb
JackX18 <nicolasgomes56@outlook.com>
2021-07-08 21:44:00 -0300

    Aula 23 -  Componente Card 02 (FlexBox)

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,35 @@ import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import { Card } from './components/Layout/Card';
 
+import './App.css'
+
 export default (props) => {
     return (
-        <div id='app'>
+        <div className='App'>
             <h1>Fundamentos React</h1>
 
-            <Card titulo="#04 - Desafio Aleatorio">
-                <Aleatorio min={1} max={60} />
-            </Card>
+            <div className="Cards">
+                <Card titulo="#04 - Desafio Aleatorio">
+                    <Aleatorio min={1} max={60} />
+                </Card>
 
-            <Card titulo="#03 - Fragmento">
-                <Fragmento />
-            </Card>
+                <Card titulo="#03 - Fragmento">
+                    <Fragmento />
+                </Card>
 
-            <Card titulo="#02 - Com Parametro">
-                <ComParametro
-                    titulo='Situação do Aluno'
-                    aluno='Pedro Silva'
-                    nota={8.7}
-                />
-            </Card>
+                <Card titulo="#02 - Com Parametro">
+                    <ComParametro
+                        titulo='Situação do Aluno'
+                        aluno='Pedro Silva'
+                        nota={8.7}
+                    />
+                </Card>
 
-            <Card titulo="#01 - Primeiro Componente">
-                <Primeiro />
+                <Card titulo="#01 - Primeiro Componente">
+                    <Primeiro />
 
-            </Card>
-
-
-
+                </Card>
+            </div>
         </div>
     )
 }
