5919b941529efd0e3142822b3e89b0d1c6f91844
JackX18 <nicolasgomes56@outlook.com>
2021-07-08 21:57:08 -0300

    Aula 24 Componentes Card 03 (Estilização & props)

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,16 @@ export default (props) => {
             <h1>Fundamentos React</h1>
 
             <div className="Cards">
-                <Card titulo="#04 - Desafio Aleatorio">
+                {/* Propriedades Filhos */}
+                <Card titulo="#04 - Desafio Aleatorio" color="#FA6900">
                     <Aleatorio min={1} max={60} />
                 </Card>
 
-                <Card titulo="#03 - Fragmento">
+                <Card titulo="#03 - Fragmento" color="#E94C6F">
                     <Fragmento />
                 </Card>
 
-                <Card titulo="#02 - Com Parametro">
+                <Card titulo="#02 - Com Parametro" color="#E8B71A">
                     <ComParametro
                         titulo='Situação do Aluno'
                         aluno='Pedro Silva'
@@ -30,7 +31,7 @@ export default (props) => {
                     />
                 </Card>
 
-                <Card titulo="#01 - Primeiro Componente">
+                <Card titulo="#01 - Primeiro Componente" color="#588C73">
                     <Primeiro />
 
                 </Card>
--- src/components/Layout/Card.jsx
+++ src/components/Layout/Card.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import './Card.css'
 
 export function Card(props) {
+
+    const cardStyle = {
+        backgroundColor: props.color || '#F00',
+        borderColor: props.color || '#F00',
+    }
     return (
-        <div className="Card">
+        <div className="Card" style={cardStyle}>
             <div className="Title">{props.titulo}</div>
             <div className="Content">
-                {props.children}
+                {/* Passar propriedades "Filhos" de um componente para outro a partir do (props.children) */}
+                {props.children} 
             </div>
         </div>
     )
