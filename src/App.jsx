de14023eff1a2059c8616a823255b6dbe050750f
JackX18 <nicolasgomes56@outlook.com>
2021-07-08 21:29:44 -0300

    Aula 22 - Componente Card 01

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,36 @@ import Aleatorio from './components/basicos/Aleatorio';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
+import { Card } from './components/Layout/Card';
 
 export default (props) => {
     return (
         <div id='app'>
             <h1>Fundamentos React</h1>
-            <Aleatorio min={10} max={60} />
-            <Fragmento />
-            <ComParametro
-                titulo='Situação do Aluno'
-                aluno='Pedro Silva'
-                nota={8.7}
-            />
-            <Primeiro />
+
+            <Card titulo="#04 - Desafio Aleatorio">
+                <Aleatorio min={1} max={60} />
+            </Card>
+
+            <Card titulo="#03 - Fragmento">
+                <Fragmento />
+            </Card>
+
+            <Card titulo="#02 - Com Parametro">
+                <ComParametro
+                    titulo='Situação do Aluno'
+                    aluno='Pedro Silva'
+                    nota={8.7}
+                />
+            </Card>
+
+            <Card titulo="#01 - Primeiro Componente">
+                <Primeiro />
+
+            </Card>
+
+
+
         </div>
     )
 }
---
+++ src/components/Layout/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Card.css'
+
+export function Card(props) {
+    return (
+        <div className="Card">
+            <div className="Title">{props.titulo}</div>
+            <div className="Content">
+                {props.children}
+            </div>
+        </div>
+    )
+}
